717869c0f622c76835fb73949665b8763370a9f4
Wallace Gonçalves <wallacecosme@gmail.com>
2021-07-27 21:23:17 -0300

    Project created

---
+++ project-ms/src/main/java/br/com/udemy/ProjectMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMsApplication.class, args);
+	}
+
+}
